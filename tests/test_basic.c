448feaf080da98a7a6a3da4909bb8b03a8d19f3d
mangdani282 <mangupli.dan@gmail.com>
2026-03-07 17:03:06 -0500

    Add more checks for string using malloc

--- tests/test_basic.c
+++ tests/test_basic.c
@@ -5,12 +5,16 @@
 
 int main() {
     // Create string using malloc
+    size_t str_size = sizeof(char) * 128;
     char *str = malloc(sizeof(char) * 128);
     assert(str != NULL);
     strcpy(str, "Hello, world!");
+    assert(strcmp(str, "Hello, world!") == 0);
     printf("%s\n", str);
 
     // Check header block
     block_header_t *str_header = (void*)str - HEADER_SIZE;
+    assert(str_header->size == str_size);
+    assert(str_header->in_use);
     printf("Size: %u, In Use: %d, Next: %p\n", str_header->size, str_header->in_use, str_header->next);
 }
