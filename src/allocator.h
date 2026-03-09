4de6e54824371bab890908fe03a3cc3d6dc1f200
mangdani282 <mangupli.dan@gmail.com>
2026-03-08 20:22:50 -0400

    Implement realloc() and calloc()

--- src/allocator.h
+++ src/allocator.h
@@ -1,13 +1,13 @@
 #ifndef ALLOCATOR_H
 #define ALLOCATOR_H
 
-#include <stddef.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 typedef struct block_header {
     size_t size;
     bool in_use;
-    struct block_header* next;
+    struct block_header *next;
 } block_header_t;
 
 #define HEADER_SIZE sizeof(block_header_t)
@@ -16,5 +16,7 @@ extern block_header_t *free_list;
 
 void *my_malloc(size_t size);
 void my_free(void *ptr);
+void *my_realloc(void *ptr, size_t size);
+void *my_calloc(size_t nmemb, size_t size);
 
 #endif
--- src/malloc.c
+++ src/malloc.c
@@ -1,9 +1,15 @@
+#include <stdint.h>
+#include <string.h>
 #include <sys/mman.h>
+
 #include "allocator.h"
 
 block_header_t *free_list = NULL;
 
 void *my_malloc(size_t size) {
+    // Check for edge case
+    if (size == 0) return NULL;
+
     // Check free list
     block_header_t *prev = NULL;
     for (block_header_t *p = free_list; p != NULL; p = p->next) {
@@ -12,12 +18,14 @@ void *my_malloc(size_t size) {
             continue;
         }
 
-        if (p == free_list) free_list = p->next;
-        else prev->next = p->next;
-        
+        if (p == free_list)
+            free_list = p->next;
+        else
+            prev->next = p->next;
+
         p->in_use = true;
         p->next = NULL;
-        return (void*)p + HEADER_SIZE;
+        return (void *)p + HEADER_SIZE;
     }
 
     // If no block exists then ask system for more memory
@@ -27,15 +35,51 @@ void *my_malloc(size_t size) {
     block->in_use = true;
     block->next = NULL;
 
-    return (void*) block + HEADER_SIZE;
+    return (void *)block + HEADER_SIZE;
 }
 
 void my_free(void *ptr) {
-    //Get header and mark it as free
-    block_header_t *header = (void*)ptr - HEADER_SIZE;
+    // Get header and mark it as free
+    block_header_t *header = (void *)ptr - HEADER_SIZE;
     header->in_use = false;
 
     // Add block back to free list
     header->next = free_list;
     free_list = header;
+}
+
+void *my_realloc(void *ptr, size_t size) {
+    // Check edge case
+    if (ptr == NULL) return my_malloc(size);
+    if (size == 0) {
+        my_free(ptr);
+        return NULL;
+    }
+
+    // Create new block of memory
+    void *new_ptr = my_malloc(size);
+    block_header_t *old_header = (void *)ptr - HEADER_SIZE, *new_header = (void *)new_ptr - HEADER_SIZE;
+
+    // Move memory over
+    size_t move_size = (old_header->size < new_header->size) ? old_header->size : new_header->size;
+    memcpy(new_ptr, ptr, move_size);
+
+    // Free old block
+    my_free(ptr);
+
+    return new_ptr;
+}
+
+void *my_calloc(size_t nmemb, size_t size) {
+    // Handle overflow case
+    if (size != 0 && nmemb > SIZE_MAX / size) return NULL;
+
+    // Create block
+    size_t total_size = nmemb * size;
+    void *ptr = my_malloc(total_size);
+
+    // Fill with zeroes
+    memset(ptr, 0, total_size);
+
+    return ptr;
 }
