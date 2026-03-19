7bf06a696ab9221b2447e5bb5a230771778f3682
mangdani282 <mangupli.dan@gmail.com>
2026-03-18 22:05:06 -0400

    Format consistently and add splitting to malloc()

--- src/malloc.c
+++ src/malloc.c
@@ -1,35 +1,69 @@
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/mman.h>
 
 #include "allocator.h"
 
-block_header_t *free_list = NULL;
+block_header_t *free_lists[3];
+
+size_t get_list_ind(size_t size) {
+    // 0-16 bytes -> free_list[0]
+    // 17-64 bytes -> free_list[1]
+    // 65+ bytes -> free_list[2]
+    if (size <= 16)
+        return 0;
+    else if (size <= 64)
+        return 1;
+    else
+        return 2;
+}
 
 void *my_malloc(size_t size) {
     // Check for edge case
     if (size == 0) return NULL;
 
+    // Get relevant free list
+    size_t ind = get_list_ind(size);
+
     // Check free list
     block_header_t *prev = NULL;
-    for (block_header_t *p = free_list; p != NULL; p = p->next) {
+    for (block_header_t *p = free_lists[ind]; p != NULL; p = p->next) {
         if (p->size < size) {
             prev = p;
             continue;
         }
 
-        if (p == free_list)
-            free_list = p->next;
+        // Prepare for giving out block
+        if (p == free_lists[ind])
+            free_lists[ind] = p->next;
         else
             prev->next = p->next;
 
+        // Split block if possible
+        if (p->size > size + HEADER_SIZE) {
+            block_header_t *split = (void *)p + HEADER_SIZE + size;
+            split->size = p->size - size - HEADER_SIZE;
+            split->in_use = false;
+
+            // Add split block to corresponding free list
+            size_t split_ind = get_list_ind(split->size);
+            split->next = free_lists[split_ind];
+            free_lists[split_ind] = split;
+
+            // Change current block size
+            p->size = size;
+        }
+
         p->in_use = true;
         p->next = NULL;
+
         return (void *)p + HEADER_SIZE;
     }
 
     // If no block exists then ask system for more memory
     block_header_t *block = mmap(NULL, HEADER_SIZE + size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (block == MAP_FAILED) return NULL;
 
     block->size = size;
     block->in_use = true;
@@ -43,9 +77,12 @@ void my_free(void *ptr) {
     block_header_t *header = (void *)ptr - HEADER_SIZE;
     header->in_use = false;
 
+    // Get relevant free list
+    size_t ind = get_list_ind(header->size);
+
     // Add block back to free list
-    header->next = free_list;
-    free_list = header;
+    header->next = free_lists[ind];
+    free_lists[ind] = header;
 }
 
 void *my_realloc(void *ptr, size_t size) {
@@ -58,6 +95,7 @@ void *my_realloc(void *ptr, size_t size) {
 
     // Create new block of memory
     void *new_ptr = my_malloc(size);
+    if (new_ptr == NULL) return NULL;
     block_header_t *old_header = (void *)ptr - HEADER_SIZE, *new_header = (void *)new_ptr - HEADER_SIZE;
 
     // Move memory over
@@ -77,6 +115,7 @@ void *my_calloc(size_t nmemb, size_t size) {
     // Create block
     size_t total_size = nmemb * size;
     void *ptr = my_malloc(total_size);
+    if (ptr == NULL) return NULL;
 
     // Fill with zeroes
     memset(ptr, 0, total_size);
